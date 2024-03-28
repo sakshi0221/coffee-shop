7b7c2ed2cb3cb6d3a15ed7e193a21fe5fa0bbb2c
Sakshi@0221 <sakshi210596@gmail.com>
2024-03-28 12:39:29 +0530

    Fixed url

--- src/App.js
+++ src/App.js
@@ -30,7 +30,7 @@ function App() {
             </div>
             <ul className={`menu-links ${isMobileMenuOpen ? "mobile-menu-open" : ""}`}>
               <span id="close-menu-btn" className="fas fa-times" onClick={toggleMobileMenu} />
-              <li><Link to="/coffee-shop" onClick={toggleMobileMenu}>Home</Link></li>
+              <li><Link to="/" onClick={toggleMobileMenu}>Home</Link></li>
               <li><Link to="/about" onClick={toggleMobileMenu}>About</Link></li>
               <li><Link to="/menu" onClick={toggleMobileMenu}>Menu</Link></li>
               <li><Link to="/contact" onClick={toggleMobileMenu}>Contact</Link></li>
